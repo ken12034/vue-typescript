5f526e7653d9d5e3a25362647eeced349749f4fe
ken12034 <ken12034@gmail.com>
2018-09-16 14:06:21 +0800

    create vue and typescript

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{title}}
+  </div>
+</template>
+
+
+<script lang="ts">
+
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class HelloDecorator extends Vue {
+  
+  public title = "123";
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,133 @@
+const path = require('path');
+const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+// using webpack-merge so we don't have to repeat common configuration attributes twice
+const merge = require('webpack-merge');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: { 
+    'main': './src/main.ts'
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['.ts', '.js', '.vue'],
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
+    },
+  },
+  output: {
+    filename: '[name].js',
+    publicPath: '/dist/',
+    //libraryTarget: 'commonjs2',
+    path: path.join(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/'),
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: __dirname,
+        exclude: file => (
+          /node_modules/.test(file) &&
+          !/\.vue\.js/.test(file)
+        )
+      },
+
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+          transpileOnly: true,
+        },
+      },
+
+      {
+        test: /\.vue$/,
+        use: {
+          loader: 'vue-loader',
+          options: {
+            url: false,
+            postLoaders: {
+              html: 'babel-loader',
+            },
+            publicPath: './src/',
+          },
+        },
+      },
+
+      {
+        test:  /\.(css|sass|scss)$/,
+        oneOf: [
+          // this matches `<style module>`
+          {
+            resourceQuery: /module/,
+            use: [
+              
+              
+              //'vue-style-loader',
+              MiniCssExtractPlugin.loader,
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: true,
+                  localIdentName: '[local]_[hash:base64:5]'
+                }
+              },
+              
+              {
+                loader: 'sass-loader',
+                options: {
+                    sourceMap: true
+                }
+              }
+            ]
+          },
+          // this matches plain `<style>` or `<style scoped>`
+          {
+            use: [
+              'vue-style-loader',
+              'css-loader'
+            ]
+          }
+        ]
+
+      },/*
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          {
+            loader: 'css-loader',
+            options: { modules: true }
+          },
+          'sass-loader'
+        ]
+      }*/
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'public/css/main.css',
+    }),
+    new HtmlWebPackPlugin({
+      template: './index.html',
+      filename: './index.html',
+    }),
+    new VueLoaderPlugin()
+  ]
+
+
+
+}
